b89c99f96204cf82fa72c8d2ac075e89073e32c0
JeissonAntunez <140208715+JeissonAntunez@users.noreply.github.com>
2024-09-25 21:09:36 -0500

    Avance 25

---
+++ Base/ADXJS/12Datos.js
@@ -0,0 +1,16 @@
+// Tipo de datos primitivos
+
+let texto = 'texto';
+let numero = 3.54545;
+let booleano = true;
+let indefinido;
+// No muy utilizados
+
+let bigInt = BigInt(3546464646466); 
+
+let jeisson=(x,a)=>{
+    return x+a;
+};
+
+console.log(jeisson(3,5));
+
